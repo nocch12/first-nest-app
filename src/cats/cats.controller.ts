c04724a782ea4ca9d4b29702711db919c793d123
Kohki Ohno <k.ohno12.gt@gmail.com>
2021-09-14 00:35:42 +0900

    cats.controllerの作成

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Header,
+  HttpCode,
+  Param,
+  Post,
+  Query,
+  Redirect,
+} from '@nestjs/common';
+import { CreateCatDto } from './create-cat.dto';
+import { ListAllEntities } from './list-all-entities.dto';
+
+@Controller('cats')
+export class CatsController {
+  @Post()
+  @HttpCode(204)
+  @Header('Cache-Control', 'none')
+  async create(@Body() CreateCatDto: CreateCatDto) {
+    console.log(CreateCatDto.age);
+    return 'created cat';
+  }
+
+  @Get()
+  // @Redirect('https://nestjs.com', 301)
+  // getDocs(@Query('veision') version: string) {
+  //   if (version && version === '5') {
+  //     return { url: 'https://docs.nestjs.com/v5/' };
+  //   }
+  // }
+  findAll(@Query() query: ListAllEntities) {
+    return `find all ${query.limit}`;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `return #${id} cat`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `deleted #${id}`;
+  }
+}
